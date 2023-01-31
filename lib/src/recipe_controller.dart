a78fa78373c80749c2b02f40ea6a197b9ee5e92d
Felix Schlegel <felix.schlegel@gmx.de>
2023-01-31 22:22:56 +0100

    feat: Added language header

--- lib/src/recipe_controller.dart
+++ lib/src/recipe_controller.dart
@@ -1,9 +1,11 @@
+import 'dart:io' show Platform;
+
 import 'package:html/dom.dart' show Document;
 import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 
 import 'recipe-scripts/kptncook.dart';
-import 'recipe_models.dart';
+import 'recipe_models.dart' show Recipe, RecipeInfo;
 
 final Map<String, Recipe Function(Document, int)> _recipeParseMethodMap = {
   'mobile.kptncook.com': parseKptnCookRecipe,
@@ -16,21 +18,29 @@ final Map<String, Recipe Function(Document, int)> _recipeParseMethodMap = {
 /// servings. The list of the parsed [Recipe]s is returned.
 Future<List<Recipe>> collectRecipes(List<RecipeInfo> recipeInfos) async {
   var results = <Recipe>[];
-
   var client = http.Client();
 
+  // Get first part of local language e.g. en_US -> en
+  var localLanguage = Platform.localeName.split("_")[0];
+  var headers = <String, String>{
+    'Accept-Language': localLanguage,
+  };
+
   for (var recipe in recipeInfos) {
-    var resultRecipe = await _collectRecipe(client, recipe);
+    var resultRecipe = await _collectRecipe(client, recipe, headers);
     results.add(resultRecipe);
   }
 
   client.close();
-
   return results;
 }
 
-Future<Recipe> _collectRecipe(http.Client client, RecipeInfo recipe) async {
-  var response = await client.get(recipe.url);
+Future<Recipe> _collectRecipe(
+  http.Client client,
+  RecipeInfo recipe,
+  Map<String, String> headers,
+) async {
+  var response = await client.get(recipe.url, headers: headers);
 
   var document = parse(response.body);
 
