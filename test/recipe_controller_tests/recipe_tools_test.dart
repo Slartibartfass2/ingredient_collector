7569377591ebcc38a3cc33fe1828b7ce95075d6b
Felix Schlegel <felix.schlegel@gmx.de>
2023-06-08 15:25:44 +0200

    test: Added more test for modifyRecipe

--- test/recipe_controller_tests/recipe_tools_test.dart
+++ test/recipe_controller_tests/recipe_tools_test.dart
@@ -75,5 +75,74 @@ void main() {
       expect(ingredient.amount, 5);
       expect(ingredient.unit, "kg");
     });
+
+    test(
+      'When modification contains ingredient with negative amount, then the '
+      'ingredient is removed from the recipe',
+      () {
+        var recipe = const Recipe(
+          ingredients: [
+            Ingredient(amount: 1, unit: "g", name: "Test Ingredient"),
+          ],
+          name: "Test Recipe",
+          servings: 2,
+        );
+
+        var modification = const RecipeModification(
+          servings: 4,
+          modifiedIngredients: [
+            Ingredient(amount: -1, unit: "kg", name: "Test Ingredient"),
+          ],
+        );
+
+        var modifiedRecipe = modifyRecipe(
+          recipe: recipe,
+          modification: modification,
+        );
+
+        expect(modifiedRecipe.name, "Test Recipe");
+        expect(modifiedRecipe.servings, 2);
+        expect(modifiedRecipe.ingredients, isEmpty);
+      },
+    );
+
+    test(
+      'When the modifications contains new ingredients, then they are added to '
+      'the recipe',
+      () {
+        var recipe = const Recipe(
+          ingredients: [
+            Ingredient(amount: 1, unit: "g", name: "Test Ingredient"),
+          ],
+          name: "Test Recipe",
+          servings: 2,
+        );
+
+        var modification = const RecipeModification(
+          servings: 4,
+          modifiedIngredients: [
+            Ingredient(amount: 10, unit: "kg", name: "Test Ingredient"),
+            Ingredient(amount: 1, unit: "kg", name: "New Ingredient"),
+          ],
+        );
+
+        var modifiedRecipe = modifyRecipe(
+          recipe: recipe,
+          modification: modification,
+        );
+
+        expect(modifiedRecipe.name, "Test Recipe");
+        expect(modifiedRecipe.servings, 2);
+        expect(modifiedRecipe.ingredients.length, 2);
+        var ingredient = modifiedRecipe.ingredients.first;
+        expect(ingredient.name, "Test Ingredient");
+        expect(ingredient.amount, 5);
+        expect(ingredient.unit, "kg");
+        ingredient = modifiedRecipe.ingredients.last;
+        expect(ingredient.name, "New Ingredient");
+        expect(ingredient.amount, 0.5);
+        expect(ingredient.unit, "kg");
+      },
+    );
   });
 }
