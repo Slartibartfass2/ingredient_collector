37b2e733b2e4d72d76172f2b4ac9d702cb2e14f1
Felix Schlegel <40503329+Slartibartfass2@users.noreply.github.com>
2025-02-27 15:03:25 +0100

    fix: fix Android build (#207)
    * ci: add build jobs
    
    * fix: fix android release
    
    * fix: fix android release
    
    * ci: cleanup

--- android/app/src/main/kotlin/com/slartibartfass2/ingredient_collector/MainActivity.kt
+++ android/app/src/main/kotlin/com/slartibartfass2/ingredient_collector/MainActivity.kt
@@ -2,5 +2,4 @@ package com.slartibartfass2.ingredient_collector
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
